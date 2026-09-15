2027e36eaf4470e94810a0f846699a51904b80fd
Eugen Scutaru <evgensc5@gmail.com>
2026-09-15 20:23:01 +0300

    add

--- src/main/java/pixel/academy/thymeleaf_app/controller/HelloWorldController.java
+++ src/main/java/pixel/academy/thymeleaf_app/controller/HelloWorldController.java
@@ -1,15 +1,20 @@
 package pixel.academy.thymeleaf_app.controller;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     //metoda pentru a afisa fprmularul HTML initial
 
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
 
@@ -17,6 +22,17 @@ public class HelloWorldController {
     // metoda se ocupa de procesarea datelor din formularu HTML
     @GetMapping("/processForm")
     public String procesForm() {
+        return "helloworld";
+    }
+    @GetMapping("/greetingNewForm")
+    public String generateGreeting(@RequestParam("studentName")String theName, Model model) {
+
+
+        theName = theName.toUpperCase();
+
+        String result = "Submission successful. User: " +theName;
+
+        model.addAttribute("message", result);
         return "helloworld";
     }
 
